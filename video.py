6f7fbc87fb6fe56a84dd8e176a4c8692ec9d11c9
Ja Xian Chen <jxc4@njit.edu>
2023-10-24 22:58:00 -0400

    Milestone 1

--- video.py
+++ video.py
@@ -1,14 +1,21 @@
-from pathlib import Path
 from pytube import YouTube
 
 file = open('links.txt','r')
 links = file.readlines()
 
 videos = "videos"
-captions = "caption"
+captions = "captions"
 
 for url in links:
     yt = YouTube(url)
+    title = yt.title
     stream = yt.streams.get_by_itag(18)
     stream.download(videos)
-    print("Downloaded:",yt.title)
+
+    caption = yt.captions.get_by_language_code('en')
+    filename = title.replace("\"", "").replace(":", "").replace(" ", "")+".txt"
+    writeFile = open(f"{captions}\\{filename}", "w+")
+    writeFile.write(caption.xml_captions)
+    writeFile.close()
+    
+    print("Downloaded:", title)
