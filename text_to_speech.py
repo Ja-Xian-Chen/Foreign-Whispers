fa7d4675522229b23d54f13be3cff796739cf3aa
Ja <Jaxianmc@gmail.com>
2023-11-25 15:41:19 -0500

    fix "-"

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,6 +3,10 @@ import pandas as pd
 import torch
 from TTS.api import TTS
 
+def preprocess_text(text):
+    # Add any necessary preprocessing for text here
+    return text.lstrip('-')
+
 def text_to_speech(input_folder, output_folder):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     # Initialize TTS model
@@ -19,14 +23,17 @@ def text_to_speech(input_folder, output_folder):
             df = pd.read_csv(os.path.join(input_folder, file_name))
             
             for index, row in df.iterrows():
-                # Check if the column is not empty
+                # Check if the "translated_text" column is not empty
                 if pd.notna(row["translated_text"]) and row["translated_text"].strip() != "":
+                    # Preprocess text if necessary
+                    preprocessed_text = preprocess_text(row["translated_text"])
                     # WAV file path
                     wav_file_path = os.path.join(csv_output_folder, f"{index}.wav")
                     # Convert text to speech and save to the WAV file
-                    tts.tts_to_file(text=row["translated_text"], file_path=wav_file_path)
+                    tts.tts_to_file(text=preprocessed_text, file_path=wav_file_path)
 
 # Example usage
 input_folder = "data/target/translated"
 output_folder = "data/target/speech"
 text_to_speech(input_folder, output_folder)
+
