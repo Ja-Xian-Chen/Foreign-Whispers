f8e45b4e8cf185b5918e345a35fdf73d19b696f8
Ja <Jaxianmc@gmail.com>
2023-11-25 15:04:26 -0500

    Sub folders

--- text_to_speech.py
+++ text_to_speech.py
@@ -5,18 +5,28 @@ from TTS.api import TTS
 
 def text_to_speech(input_folder, output_folder):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    tts = TTS(model_name="tts_models/de/thorsten/tacotron2-DDC").to(device)    
+    # Initialize TTS model
+    tts = TTS(model_name="tts_models/de/thorsten/tacotron2-DDC").to(device)
     os.makedirs(output_folder, exist_ok=True)
 
     for file_name in os.listdir(input_folder):
         if file_name.endswith(".csv"):
+            csv_name = os.path.splitext(file_name)[0]
+            csv_output_folder = os.path.join(output_folder, csv_name)
+            
+            # Create output folder 
+            os.makedirs(csv_output_folder, exist_ok=True)
             df = pd.read_csv(os.path.join(input_folder, file_name))
-
+            
             for index, row in df.iterrows():
-                wav_file_path = os.path.join(output_folder, f"{index}.wav")
-                tts.tts_to_file(text = row["translated_text"], file_path = wav_file_path)
+                # Check if the column is not empty
+                if pd.notna(row["translated_text"]) and row["translated_text"].strip() != "":
+                    # WAV file path
+                    wav_file_path = os.path.join(csv_output_folder, f"{index}.wav")
+                    # Convert text to speech and save to the WAV file
+                    tts.tts_to_file(text=row["translated_text"], file_path=wav_file_path)
 
-if __name__ == "__main__":
-    input_folder = "data/target/translated"
-    output_folder = "data/target/speech"
-    text_to_speech(input_folder, output_folder)
+# Example usage
+input_folder = "data/target/translated"
+output_folder = "data/target/speech"
+text_to_speech(input_folder, output_folder)
