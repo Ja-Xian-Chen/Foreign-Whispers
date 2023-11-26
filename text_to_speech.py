7e29ab1a15778874a8257ed82b50390e7d8ce233
Ja <Jaxianmc@gmail.com>
2023-11-25 21:24:19 -0500

    fixed the dashes not working

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,9 +3,8 @@ import pandas as pd
 import torch
 from TTS.api import TTS
 
-def preprocess_text(text):
-    # Add any necessary preprocessing for text here
-    return text.lstrip('-')
+def remove_dashes(text):
+    return text.replace("-", " ")
 
 def text_to_speech(input_folder, output_folder):
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -22,18 +21,17 @@ def text_to_speech(input_folder, output_folder):
             os.makedirs(csv_output_folder, exist_ok=True)
             df = pd.read_csv(os.path.join(input_folder, file_name))
             
+            df["translated_text"] = df["translated_text"].apply(lambda x: remove_dashes(str(x)) if pd.notna(x) else x)
+            
             for index, row in df.iterrows():
-                # Check if the "translated_text" column is not empty
+                # Check if the column is not empty
                 if pd.notna(row["translated_text"]) and row["translated_text"].strip() != "":
-                    # Preprocess text if necessary
-                    preprocessed_text = preprocess_text(row["translated_text"])
                     # WAV file path
                     wav_file_path = os.path.join(csv_output_folder, f"{index}.wav")
                     # Convert text to speech and save to the WAV file
-                    tts.tts_to_file(text=preprocessed_text, file_path=wav_file_path)
+                    tts.tts_to_file(text=row["translated_text"], file_path=wav_file_path)
 
 # Example usage
 input_folder = "data/target/translated"
 output_folder = "data/target/speech"
 text_to_speech(input_folder, output_folder)
-
